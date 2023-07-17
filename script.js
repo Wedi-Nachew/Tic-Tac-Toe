d32616584b9073eafa7510612c22f9d21ca8f523
Filomn Abrha <filmonabrha2020@gmail.com>
2023-07-17 11:25:07 +0300

    Set the player mark one-by-one the gameDisplay

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const GameBoard = (()=>{
     const gameDisplay = document.querySelector(".game-board")
-    const gameBoard = ["X", "O", "X", "O", "X", "O", "O", "X", "O"]
+    const gameBoard = ["", "", "", "", "", "", "", "", ""]
     const newArr = []
-    
+    let count = 1;
+
     gameBoard.filter((item, index)=> newArr.push(index))
     const renderContents = ()=>{
         for(let i=0; i< gameBoard.length; i++){
@@ -14,18 +15,39 @@ const GameBoard = (()=>{
     }
     const clickEvent = ()=>{ 
         gameDisplay.addEventListener("click", (event)=>{
-            if(event.target.nodeName === "DIV"){
-                console.log(event.target.dataset.index)
-
-            }
+            getPlayerMark(event)
+            renderContents()
         })
 
     }
-    // function setPlayerMark(){
-    //     for(let i=0; i< gameBoard.length; i++){
-            
-    //     }
-    // }
+    function getPlayerMark(event){
+        if(count % 2 !== 0){
+            firstPlayer.setPlayerMark(event)
+            count++
+        } else if(count % 2 === 0){
+            secondPlayer.setPlayerMark(event)
+            count++
+        }
+    }
+   
     renderContents()
     clickEvent()
+
+    return{gameBoard, gameDisplay}
 })();
+const Player = (mark)=>{
+    let count = 1;
+    const setPlayerMark = (event)=>{
+        while(GameBoard.gameDisplay.firstChild){
+            GameBoard.gameDisplay.removeChild(GameBoard.gameDisplay.firstChild)}
+            GameBoard.gameBoard[event.target.dataset.index] = mark       
+    }
+
+
+
+    return{setPlayerMark}
+
+}
+
+const firstPlayer = Player("X")
+const secondPlayer = Player("O")
